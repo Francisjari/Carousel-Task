9a5b09bd91976a8cd1b49b8710e5dd791b3f4ebf
Francis Jari <kitgakjari@gmail.com>
2022-11-11 14:16:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; }
+    slides[slideIndex-1].style.display = "block"; 
+  
+}
+ const btns = document.querySelectorAll(".btn")
+ const slider= document.querySelector(".container")
+
+slider.addEventListener("mouseover", () => {
+  btns.forEach((btn) => {
+    btn.style.display = "block";
+  });
+});
+
+slider.addEventListener("mouseout", () => {
+  btns.forEach((btn) => {
+    btn.style.display = "none";
+  });
+});
